8d4fbb64d87b9cacb81eac7e8987624d311ed569
Amanda Gxagxa <amandagxagxa@gmail.com>
2018-10-15 16:02:54 +0200

    connected get route woking

--- routes/waiterRouts.js
+++ routes/waiterRouts.js
@@ -1,27 +1,13 @@
 module.exports = function (waiterSev){
-    async function addWaiter(req, res) {
-        try {
-            let names = req.params.names;
-            let name = names.toUpperCase();
-            console.log(names);
-
-                req.flash('info',  'Hello ' + name + ', please select which days will you be available in');
-                waiterResults = await waiterSev.createOrAddWaiter(name);
-                console.log(waiterResults)
-            // let count = await services.selectCount();
-            res.redirect('weekdays', { waiterResults });
-        } catch (err) {
-            res.send(err.stack);
-        }
-    }
+    
     async function selectDays(req, res) {
         try {
             
             
             let names = req.params.names;
             let name = names.toUpperCase();
-            console.log(names+" nali gama");
-            req.flash('info',  'Hello ' + name + ', please select which days will you be available in');
+            // console.log(names+" nali gama");
+            req.flash('info',  'Hello ' + name + ', please select which days will you be available in.');
             let weekdaysResults = await waiterSev.getDays();
             let  weekdays = weekdaysResults;
             //console.log(weekdays);
@@ -35,7 +21,21 @@ module.exports = function (waiterSev){
         }
       }
       
-      
+      async function addWaiter(req, res) {
+        try {
+            let names = req.params.names;
+            let name = names.toUpperCase();
+            console.log(names);
+
+                req.flash('info',  'Hello ' + name + ', please select which days will you be available in.');
+                waiterResults = await waiterSev.createOrAddWaiter(name);
+                console.log(waiterResults)
+            // let count = await services.selectCount();
+            res.redirect('weekdays', { waiterResults });
+        } catch (err) {
+            res.send(err.stack);
+        }
+    }
 
     
       return{
